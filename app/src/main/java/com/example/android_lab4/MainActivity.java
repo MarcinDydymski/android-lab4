09e69f55530178f415c6d0f9e639bc265a021a09
Marcin Dydymski <marcin_dydymski@poczta.fm>
2020-05-16 16:54:56 +0200

    Tworzenie formularza dodawania wpisu

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.android_lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> target;
     private ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,4 +44,20 @@ public class MainActivity extends AppCompatActivity {
         //TODO
         //Kolejny komentarz
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
 }
